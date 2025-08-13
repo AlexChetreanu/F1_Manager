f902cca7c8a80c1c8a9418697c3941b12b3b35ae
AlexChetreanu <92926637+AlexChetreanu@users.noreply.github.com>
2025-08-13 19:46:42 +0300

    Refresh historic race data on year change

--- F1App/F1App/HistoricRaceViewModel.swift
+++ F1App/F1App/HistoricRaceViewModel.swift
@@ -7,14 +7,17 @@ class HistoricRaceViewModel: ObservableObject {
         guard let url = URL(string: "http://localhost:8000/api/races?year=\(year)&circuit_id=\(circuitId)") else { return }
 
         URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                if let decoded = try? JSONDecoder().decode([Race].self, from: data) {
-                    DispatchQueue.main.async {
-                        self.race = decoded.first
-                    }
-                }
-            } else if let error = error {
-                print("Error fetching historic race:", error)
+            guard error == nil, let data = data else {
+                DispatchQueue.main.async { self.race = nil }
+                if let error = error { print("Error fetching historic race:", error) }
+                return
+            }
+
+            if let decoded = try? JSONDecoder().decode([Race].self, from: data),
+               let race = decoded.first {
+                DispatchQueue.main.async { self.race = race }
+            } else {
+                DispatchQueue.main.async { self.race = nil }
             }
         }.resume()
     }
