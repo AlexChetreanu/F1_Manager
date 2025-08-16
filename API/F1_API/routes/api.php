8fc89197bc79e9fdeacba615029295c4b6f6f630
AlexChetreanu <92926637+AlexChetreanu@users.noreply.github.com>
2025-08-16 20:22:49 +0300

    Add route and artisan command to fetch OpenF1 driver data

--- API/F1_API/routes/api.php
+++ API/F1_API/routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\DriverController;
 use App\Http\Controllers\RaceController;
 use App\Http\Controllers\OpenF1Controller;
+use App\Http\Controllers\DataController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -18,6 +19,7 @@ Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
 });
 Route::middleware('auth:sanctum')->put('/password', [PasswordController::class, 'update_app']);
 Route::get('/drivers', [DriverController::class, 'index']);
+Route::post('/drivers/fetch', [DataController::class, 'fetchData']);
 
 Route::get('/races', [RaceController::class, 'apiIndex'])->name('races.api');
 
