ed1415548b1575820dde53c83d93d2f5a3d23666
AlexChetreanu <92926637+AlexChetreanu@users.noreply.github.com>
2025-08-22 03:47:06 +0300

    logo

--- F1App/F1App/Views/Splash/VectorSplashView.swift
+++ F1App/F1App/Views/Splash/VectorSplashView.swift
@@ -8,7 +8,7 @@ struct VectorSplashView: View {
         ZStack {
             Color.black.ignoresSafeArea()
 
-            // Logo vectorial (fără imagini)
+            // Logo vectorial
             F1VectorLogo()
                 .scaleEffect(appear ? 1.0 : 0.85)
                 .opacity(appear ? 1.0 : 0.0)
@@ -17,9 +17,10 @@ struct VectorSplashView: View {
         .onAppear {
             appear = true
             // Finalizează splash-ul după ~1.2s
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 withAnimation(.easeInOut(duration: 0.25)) { onFinish() }
             }
+        
         }
     }
 }
