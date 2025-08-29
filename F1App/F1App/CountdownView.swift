06823ae3c9063159936c79434347550d40820deb
AlexChetreanu <92926637+AlexChetreanu@users.noreply.github.com>
2025-08-29 15:49:56 +0300

    Revert "Refine countdown timer styling"

--- F1App/F1App/CountdownView.swift
+++ F1App/F1App/CountdownView.swift
@@ -7,11 +7,8 @@ struct CountdownView: View {
 
     var body: some View {
         Text(formattedTime)
-            .font(.custom("New York", size: 34))
+            .font(.largeTitle)
             .padding()
-            .background(Color.red)
-            .foregroundColor(.white)
-            .cornerRadius(8)
             .overlay(
                 RoundedRectangle(cornerRadius: 8)
                     .stroke(Color.black, lineWidth: 2)
